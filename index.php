b28511573c33d7f5e87975ef70bf70bb7e89b184
angelapg23 <52908295+angelapg23@users.noreply.github.com>
2020-06-02 17:16:57 -0600

    creando index
    creacion pagina index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <h1> Ejercicios de PHP </h1>
+    <ul>
+        <li><a href="" target="_new">Ejercicio 2 </a></li>
+        <li><a href="" target="_new">Ejercicio 3 </a></li>
+        <li><a href="" target="_new">Ejercicio 4 </a></li>
+        <li><a href="" target="_new">Ejercicio 5 </a></li>
+        <li><a href="" target="_new">Ejercicio 6 </a></li>
+</ul>
+</body>
+</html>
