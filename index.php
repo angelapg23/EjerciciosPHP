665d4757081404c7c23ad229f6ebc06dfefd8182
angelapg23 <52908295+angelapg23@users.noreply.github.com>
2020-06-02 18:51:20 -0600

    Añadiendo mas archivos
    ejercicio4

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <ul>
         <li><a href="ejercicio2.php" target="_new">Ejercicio 2 </a></li>
         <li><a href="ejercicio3.php" target="_new">Ejercicio 3 </a></li>
-        <li><a href="" target="_new">Ejercicio 4 </a></li>
+        <li><a href="ejercicio4.php" target="_new">Ejercicio 4 </a></li>
         <li><a href="" target="_new">Ejercicio 5 </a></li>
         <li><a href="" target="_new">Ejercicio 6 </a></li>
 </ul>
